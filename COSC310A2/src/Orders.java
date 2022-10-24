b3af3b4381ec182f51a65b8d89b9849e536b8154
matthewmarchant <114941298+matthewmarchant@users.noreply.github.com>
2022-10-24 13:41:42 -0700

    Added Order Screen

---
+++ COSC310A2/src/Orders.java
@@ -0,0 +1,108 @@
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Orders {
+    private JFormattedTextField ManagerTitle;
+    private JButton logOutButton;
+    private JButton homeButton;
+    private JTable OrderTable;
+    private JFormattedTextField supplierFormattedTextField;
+    private JComboBox supplierDropDown;
+    JPanel OrderScreen;
+    private JButton clearOrderButton;
+    private JButton submitOrderButton;
+
+
+    public Orders() {
+        DBConnection con = new DBConnection();
+        int[] supplierIds = con.getAllSuppliers();
+        String[] suppliers = new String[supplierIds.length];
+        int i = 0;
+        for(int id : supplierIds){
+            suppliers[i++] = con.getSupplierNameById(id);
+        }
+        supplierDropDown.setModel(new DefaultComboBoxModel(suppliers));
+        DefaultTableModel model = new DefaultTableModel() {
+            boolean[] canEdit = new boolean[]{
+                    false, false, false, false, true
+            };
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit[columnIndex];
+            }
+        };
+        OrderTable.setModel(model);
+        model.addColumn("ID");
+        model.addColumn("Name");
+        model.addColumn("Price");
+        model.addColumn("Stock");
+        model.addColumn("# to Order");
+        int[] ids = con.getAllProductIds();
+        String supplier = supplierDropDown.getSelectedItem().toString();
+        for(int id : ids){
+            if(supplier.equals(con.getProductSupplier(id))) {
+                String name = con.getProductName(id);
+                double price = con.getProductPrice(id);
+                int stock = con.getProductStock(id);
+                int OrderValue = 0;
+                model.insertRow(0, new Object[]{"" + id, name, price, "" + stock, "" + OrderValue});
+            }
+        }
+        con.close();
+        homeButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Main.frame.setContentPane(new Manager().ManagerScreen);
+                Main.frame.pack();
+            }
+        });
+        logOutButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Main.frame.setContentPane(new Login().LoginPanel);
+                Main.frame.pack();
+            }
+        });
+        supplierDropDown.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                DBConnection con = new DBConnection();
+                DefaultTableModel model = new DefaultTableModel() {
+                    boolean[] canEdit = new boolean[]{
+                            false, false, false, false, true
+                    };
+                    public boolean isCellEditable(int rowIndex, int columnIndex) {
+                        return canEdit[columnIndex];
+                    }
+                };
+                OrderTable.setModel(model);
+                model.addColumn("ID");
+                model.addColumn("Name");
+                model.addColumn("Price");
+                model.addColumn("Stock");
+                model.addColumn("# to Order");
+                int[] ids = con.getAllProductIds();
+                String supplier = supplierDropDown.getSelectedItem().toString();
+                for(int id : ids){
+                    if(supplier.equals(con.getProductSupplier(id))) {
+                        String name = con.getProductName(id);
+                        double price = con.getProductPrice(id);
+                        int stock = con.getProductStock(id);
+                        int OrderValue = 0;
+                        model.insertRow(0, new Object[]{"" + id, name, price, "" + stock, "" + OrderValue});
+                    }
+                }
+                con.close();
+            }
+        });
+        clearOrderButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                for(int j=0;(model.getRowCount())>j;j++){
+                    model.setValueAt("0",j,4);
+                }
+            }
+           });
+    }
+}
