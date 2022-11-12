714bb4e3a30d3682638decc73bb1f1d501778a5d
stu <stumcg@student.ubc.ca>
2022-11-11 18:37:03 -0800

    uncommented login checks (had disabled them for easy testing)

--- COSC310A2/src/Login.java
+++ COSC310A2/src/Login.java
@@ -20,7 +20,7 @@ public class Login {
             public void actionPerformed(ActionEvent e) {
                 DBConnection con = new DBConnection();
                 try{
-                    if(true){//con.checkLogin(userNameInput.getText(), new String(passwordInput.getPassword()), true)){
+                    if(con.checkLogin(userNameInput.getText(), new String(passwordInput.getPassword()), true)){
                         con.close();
                         Main.frame.setContentPane(new Manager().ManagerScreen);
                         Main.frame.pack();
@@ -36,7 +36,7 @@ public class Login {
             public void actionPerformed(ActionEvent e) {
                 DBConnection con = new DBConnection();
                 try{
-                    if(true){//con.checkLogin(userNameInput.getText(), new String(passwordInput.getPassword()), false)){
+                    if(con.checkLogin(userNameInput.getText(), new String(passwordInput.getPassword()), false)){
                         con.close();
                         Main.frame.setContentPane(new Cashier().CashierScreen);
                         Main.frame.pack();
